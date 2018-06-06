e96a0d48e1b473d76d5a0de3e8f5de134de0c6eb
Paul Lindberg <paul.lindberg@mymail.champlain.edu>
2018-06-06 10:41:26 -0700

    Removed unused tkinter elements
    rearranged stuff to run in an environment class

--- __init__.py
+++ __init__.py
@@ -2,13 +2,8 @@ from widgets import *
 
 
 def main():
-    # root = Tk()
-    # app = Application(master=root)
-    # app.mainloop()
-    # root.destroy()
-
-    app = Application();
-    app.game();
+    env = Environment()
+    env.game()
 
 
 if __name__ == '__main__':
--- widgets.py
+++ widgets.py
@@ -1,85 +1,78 @@
+import sys
 import math
 import pygame
 import random
-from tkinter import *
 
 pygame.init()
-pygame.font.init()
+# pygame.font.init()
 
+# boundaries
 SCREEN_WIDTH, SCREEN_HEIGHT = 750, 500
-FLOOR_HEIGHT = 470
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT
+FLOOR_HEIGHT = 470
+
+# physics constants
 SPEED_DEFAULT = 0.2  # 0.2
 SPEED_SIZE_DEFAULT = 10000.0  # Makes speed vary depending upon the particle's size
 MAGNITUDE_DEFAULT = 0.002  # 0.002
 DRAG_DEFAULT = 0.9995  # 0.999
 ELASTICITY_DEFAULT = 0.75  # 0.75
+MASS_AIR_DEFAULT = 0.2  # 0.2
+
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
+# colors
 COLOR_BLACK = 0, 0, 0
 COLOR_GRAY_19 = 31, 31, 31
 COLOR_GRAY_21 = 54, 54, 54
 COLOR_GRAY_41 = 105, 105, 105
 COLOR_ORANGE = 251, 126, 20
 
-FONT_ORB_DEFAULT = pygame.font.Font('data/fonts/r_fallouty.ttf', 20)
+# fonts
+FONT_ORB_DEFAULT = pygame.font.Font('data/fonts/r_fallouty.ttf', 15)
 
+# particle dimensions
 WIDTH_PAIR_DEFAULT = 60
 WIDTH_PARTICLE_DEFAULT = 15
 
 
-def rand_color():
-    color = []
-    for x in range(3):
-        color.append(random.randint(0, 255))
-    return color
+class Environment:
+    def __init__(self, width=SCREEN_WIDTH, height=SCREEN_HEIGHT, floor_height=FLOOR_HEIGHT):
+        self.screen_width = width
+        self.screen_height = height
+        self.screen_size = self.screen_width, self.screen_height
 
-
-class Application(Frame):
-    def __init__(self, master=None):
-        self.master = master
-        Frame.__init__(self, self.master)
-
-        pi_title_pre = PhotoImage(file="sayaa_title.gif")
-        self.pi_title = Label(self, image=pi_title_pre)
-        self.pi_title.image = pi_title_pre
-        self.b_start = Button(self.master, text="START", command=self.game, font=('Fallouty', 12))
-        self.b_exit = Button(self.master, text="EXIT", command=self.escape, font=('Fallouty', 12))
-        self.widgets_bind()
-
-    def escape(self, event=None):
-        self.quit()
-
-    def game(self, event=None):
-        pygame.display.set_caption('Stand As You Are Able')
+        self.floor_height = floor_height
 
         # list to contain all particles (including individual ones inside of particle pairs):
-        particles_master = []
+        self.particles_master = []
+        # list to contain pairs
+        self.particles_pairs = []
 
-        # list to contain
-        particles_pairs = []
+    def game(self):
+        pygame.display.set_caption('Stand As You Are Able')
 
         # create particle pairs:
-        # pair_1 = self.Pair(p_width=20, pp_x=int(SCREEN_WIDTH / 2) - 60)
-        pair_2 = self.Pair(pp_x=int(SCREEN_WIDTH / 2) + 90)
+        pair_1 = self.Pair(p_width=20, pp_x=int(SCREEN_WIDTH / 2) - 60, pp_mass=100000)
+        pair_2 = self.Pair(pp_x=int(SCREEN_WIDTH / 2) + 90, pp_mass=100000)
 
         # append particle pairs to particle pair list
-        # particles_pairs.append(pair_1)
-        particles_pairs.append(pair_2)
+        self.particles_pairs.append(pair_1)
+        self.particles_pairs.append(pair_2)
 
         # append paired particles to master list
-        for pair_increment in range(len(particles_pairs)):
-            particles_master.append(particles_pairs[pair_increment].dump()[0])
-            particles_master.append(particles_pairs[pair_increment].dump()[1])
+        for pair_increment in range(len(self.particles_pairs)):
+            self.particles_master.append(self.particles_pairs[pair_increment].dump()[0])
+            self.particles_master.append(self.particles_pairs[pair_increment].dump()[1])
 
         # random background circle test (creates more particles)
         # loop to create and append particles
-        for i in range(20):
-            size = random.randint(10, 20)
-            density = random.randint(1, 20)
+        for i in range(10):
+            size = random.randint(5, 10)  # (10, 20)
+            density = random.randint(15, 20)  # (1, 20)
             x = random.randint(size, SCREEN_WIDTH - size)
             y = random.randint(size, int(SCREEN_HEIGHT / 2) - size)
-            particles_master.append(Particle(x=x, y=y, p_size=size, p_color=COLOR_GRAY_41, p_mass=density * size ** 2))
+            self.particles_master.append(Particle(x=x, y=y, p_size=size, p_color=COLOR_GRAY_41, p_mass=density * size ** 2))
 
         # update the simulation until the user exits
         while 1:
@@ -90,38 +83,34 @@ class Application(Frame):
                     sys.exit()
             # screen fill (removes blur)
             screen.fill(COLOR_BLACK)
-            # particle movement
-            for i, particle in enumerate(particles_master):
-                particle.move()
-                particle.bounce()
-                for particle2 in particles_master[i + 1:]:
-                    collide(particle, particle2)
-                particle.display()
+            self.update()
             pygame.display.update()
+
+    # particle movement
+    def update(self):
+        for i, particle in enumerate(self.particles_master):
+            particle.move()
+            particle.bounce()
+            for particle2 in self.particles_master[i + 1:]:
+                collide(particle, particle2)
+            particle.display()
 
     class Pair:
         # TODO: Create a bar between the orbs
         def __init__(self, p_width=WIDTH_PARTICLE_DEFAULT, pp_x=int(SCREEN_WIDTH / 2), pp_y=FLOOR_HEIGHT,
-                     pp_width=WIDTH_PAIR_DEFAULT, pp_color=None):
+                     pp_width=WIDTH_PAIR_DEFAULT, pp_color=None, pp_mass=1):
             if pp_color is None:
                 pp_color = rand_color()
             # adjust floor placement based on width
             self.orb_pair = []
             pp_y -= p_width
-            self.orb1 = Particle(pp_x, pp_y, p_color=pp_color, p_size=p_width)
-            self.orb2 = Particle(pp_x + pp_width, pp_y, p_color=pp_color, p_size=p_width)
+            self.orb1 = Particle(pp_x, pp_y, p_color=pp_color, p_size=p_width, p_mass=pp_mass)
+            self.orb2 = Particle(pp_x + pp_width, pp_y, p_color=pp_color, p_size=p_width, p_mass=pp_mass)
             self.orb_pair.append(self.orb1)
             self.orb_pair.append(self.orb2)
 
         def dump(self):
             return self.orb_pair
-
-    def widgets_bind(self):
-        self.pi_title.grid(row=0, column=0, columnspan=2, sticky=NW)
-        self.b_start.grid(row=1, column=0, sticky=W)
-        self.b_start.bind_all('<Return>', self.game)
-        self.b_exit.grid(row=1, column=1, sticky=E)
-        self.b_exit.bind_all('<BackSpace>', self.escape)
 
 
 class Particle:
@@ -129,15 +118,16 @@ class Particle:
                  p_speed=SPEED_DEFAULT, p_mass=1, p_text=None):
         self.x, self.y = x, y
         self.size = p_size
-        self.color = p_color
-        self.thickness = p_thickness
         self.speed = p_speed
         self.angle = p_angle
         self.mass = p_mass
+        self.drag = (self.mass/(self.mass + MASS_AIR_DEFAULT)) ** self.size
         if p_text is None:
             self.text = str(self.mass)
         else:
             self.text = p_text
+        self.color = p_color
+        self.thickness = p_thickness
 
     def bounce(self):
         if self.x > SCREEN_WIDTH - self.size:
@@ -171,7 +161,7 @@ class Particle:
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
         (self.angle, self.speed) = add_vectors(self.angle, self.speed, math.pi, MAGNITUDE_DEFAULT)
-        self.speed *= DRAG_DEFAULT
+        self.speed *= self.drag
         self.speed *= (1 - self.size / SPEED_SIZE_DEFAULT)
 
 
@@ -211,6 +201,13 @@ def collide(p1, p2):
         p2.x -= math.sin(angle) * overlap
         # p2.y += math.cos(angle)
         p2.y += math.cos(angle) * overlap
+
+
+def rand_color():
+    color = []
+    for x in range(3):
+        color.append(random.randint(0, 255))
+    return color
 
 
 # adapted from https://stackoverflow.com/questions/20842801/how-to-display-text-in-pygame
